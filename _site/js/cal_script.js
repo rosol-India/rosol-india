d4b59241bd8eafc76cc7b36883e10ec7dc4c0bd6
rosol-india <rosolindia@gmail.com>
2017-07-10 12:43:07 +0530

    updations

---
+++ _site/js/cal_script.js
@@ -0,0 +1,18 @@
+function disp(){
+	document.getElementById("first_slide").style.display="none"
+	document.getElementById("second_slide").style.display="block";
+	document.getElementById("third_slide").style.display="none";
+}
+function disp1(){
+	document.getElementById("first_slide").style.display="none"
+	document.getElementById("second_slide").style.display="none";
+	document.getElementById("third_slide").style.display="block";
+}
+function disp2(){
+	document.getElementById("first_slide").style.display="block"
+	document.getElementById("second_slide").style.display="none";
+	document.getElementById("third_slide").style.display="none";
+}
+
+
+
