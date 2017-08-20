d3a06ae1ba3782da9323af520b5e3341ea8039dc
rosol-india <rosolindia@gmail.com>
2017-08-20 14:05:19 +0530

    CHANGES IN HOME

--- js/slider.js
+++ js/slider.js
@@ -28,9 +28,9 @@ var t = 0;
           -webkit-transition: background-image 0.6s linear;
        transition: background-image 0.6s linear;
     */
-window.onload = function() {
-  setTimeout(function() { changep(); }, 4000);
+window.onload = function() {changep()
 }
+/*
 function clicka()
 {        
     clearInterval(interval);
@@ -112,17 +112,12 @@ function clicke()
        document.getElementById('c-butnd').style.display='inline-block';
        document.getElementById('c-butne').style.display='inline-block';
 }
+*/
 function changep()
 {      
        if(counter==0){
         document.getElementById("home_s").style.backgroundImage="url(/img/a.jpg)";
-        document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Sun is simply a one big battery for us.</center></span></center>';
-       document.getElementById('c-butna').style.backgroundColor='black';
-         document.getElementById('c-butna').style.display='inline-block';
-       document.getElementById('c-butnb').style.display='inline-block';
-       document.getElementById('c-butnc').style.display='inline-block';
-       document.getElementById('c-butnd').style.display='inline-block';
-       document.getElementById('c-butne').style.display='inline-block';
+        document.getElementById('p_content').innerHTML = '   <span id="chead">Rays of <span id="hsun"> Sun </span> Oriented Light</span><br><span id="ibout">A Solar Powered Products &amp  Solutions Company<span style="color:#5FAD56"> .</span></span>';
       
        counter+=1;
    }
@@ -131,56 +126,28 @@ function changep()
       { 
         counter+=1;
         //here add the background image of slide 1
-        document.getElementById("home_s").style.backgroundImage="url(/img/a.jpg)";
-         document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Sun is simply a one big battery for us.</center></span></center>';
-     document.getElementById('c-butna').style.backgroundColor='black';
-       document.getElementById('c-butne').style.backgroundColor='white';
-        document.getElementById('c-butna').style.display='inline-block';
-       document.getElementById('c-butnb').style.display='inline-block';
-       document.getElementById('c-butnc').style.display='inline-block';
-       document.getElementById('c-butnd').style.display='inline-block';
-       document.getElementById('c-butne').style.display='inline-block';
+        document.getElementById("home_s").style.backgroundImage="url(/img/b.jpg)";
+         document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Solar, the energy of the future.</center></span></center>';
       }
         else if(counter == 1){
           counter+=1;
           //here add the background image of slide 2
           document.getElementById("home_s").style.backgroundImage="url(/img/c.jpg)";
-           document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Let\'s make the world a better place.</center></span></center>';
-        document.getElementById('c-butna').style.backgroundColor='white';
-          document.getElementById('c-butnb').style.backgroundColor='black';
-           document.getElementById('c-butna').style.display='inline-block';
-       document.getElementById('c-butnb').style.display='inline-block';
-       document.getElementById('c-butnc').style.display='inline-block';
-       document.getElementById('c-butnd').style.display='inline-block';
-       document.getElementById('c-butne').style.display='inline-block';
+           document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Electricity bills are increasing by 30% every year as for sunshine its free.</center></span></center>';
 
         }
          else if(counter == 2){
           counter+=1;
           //here add the background image of slide 3
           document.getElementById("home_s").style.backgroundImage="url(/img/d.jpg)";
-           document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Our bright ideas come from the sun.</center></span></center>';
-            document.getElementById('c-butnb').style.backgroundColor='white';
-            document.getElementById('c-butnc').style.backgroundColor='black';
-             document.getElementById('c-butna').style.display='inline-block';
-       document.getElementById('c-butnb').style.display='inline-block';
-       document.getElementById('c-butnc').style.display='inline-block';
-       document.getElementById('c-butnd').style.display='inline-block';
-       document.getElementById('c-butne').style.display='inline-block';
+           document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Sun is simply  one big battery for us.</center></span></center>';
  
         }
          else if(counter == 3){
           counter+=1;
           //here add the background image of slide 4
           document.getElementById("home_s").style.backgroundImage="url(/img/e.jpg)";
-          document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>When there’s a huge solar energy spill, it’s just called a ‘nice day’</center></span></center>';
-            document.getElementById('c-butnc').style.backgroundColor='white';
-            document.getElementById('c-butnd').style.backgroundColor='black';
-             document.getElementById('c-butna').style.display='inline-block';
-       document.getElementById('c-butnb').style.display='inline-block';
-       document.getElementById('c-butnc').style.display='inline-block';
-       document.getElementById('c-butnd').style.display='inline-block';
-       document.getElementById('c-butne').style.display='inline-block';
+          document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>You can \' t buy Happiness but you can buy Solar Panels and that \' s pretty close. </center></span></center>';
  
         }
          else if(counter == 4){
@@ -189,13 +156,6 @@ function changep()
           //here add the background image of slide 5
           document.getElementById("home_s").style.backgroundImage="url(/img/f.jpg)";
            document.getElementById('p_content').innerHTML = '<center><span id="quote"><center>Solar energy, clean energy for a better tomorrow</center></span></center>';
-            document.getElementById('c-butnd').style.backgroundColor='white';
-            document.getElementById('c-butne').style.backgroundColor='black';
-             document.getElementById('c-butna').style.display='inline-block';
-       document.getElementById('c-butnb').style.display='inline-block';
-       document.getElementById('c-butnc').style.display='inline-block';
-       document.getElementById('c-butnd').style.display='inline-block';
-       document.getElementById('c-butne').style.display='inline-block';
 }
-}, 10000);
+}, 8000);
 }
